44e1daaa3cfe54e698e8c44b8e378928127ec25e
e298729 <emilio@ermilio.uam>
2017-12-18 15:03:47 +0100

    ;)

--- proceso_imagenes.py
+++ proceso_imagenes.py
@@ -8,6 +8,7 @@ from skimage.filters import median as filt_med
 from skimage.morphology import disk
 
 __umbral__ = 122
+__disk_size__= 21
 
 def cargar_numeros(folder):
     files = os.listdir(folder)
@@ -24,21 +25,26 @@ def cargar_numeros(folder):
 def intensidad_media(image):
     return np.mean(image)
 
+def intensidad_mediana(image):
+    return np.median(image)
+
 def umbralizar_imagen(imagen):
     imagen_2 = deepcopy(imagen)
 
     imagen_2[imagen>__umbral__] = 255
     imagen_2[imagen<=__umbral__] = 0
 
-    return imagen_2
+    return filt_med(imagen_2, disk(__disk_size__))
 
 def intensidad_media_umbralizada(imagen):
     return intensidad_media(umbralizar_imagen(imagen))
 
-def obtener_esqueleto(imagen):
-    gray = filt_med(imagen, disk(7))
+def intensidad_mediana_umbralizada(imagen):
+    return intensidad_mediana(umbralizar_imagen(imagen))
 
-    image_2 = skeletonize_3d((255 - umbralizar_imagen(gray)) / 255)
+def obtener_esqueleto(imagen):
+
+    image_2 = skeletonize_3d((255 - umbralizar_imagen(imagen)) / 255)
     image_2[:30, :] = 0
     image_2[-30:, :] = 0
     image_2[:, :30] = 0
@@ -52,6 +58,45 @@ def contar_rectas(imagen):
     _array, _, _ = hough_line_peaks(h, theta, d)
 
     return _array.shape[0]
+
+
+def proyeccion_y(imagen):
+    umbralizada = umbralizar_imagen(imagen)
+
+    return np.fromiter(map(np.sum,255 - umbralizada),dtype=int)
+
+
+def proyeccion_x(imagen):
+    return proyeccion_y(imagen.T)
+
+def alto_numero(imagen):
+    proyeccion = proyeccion_y(imagen)
+
+    index_floor = 0
+    index_top = imagen.shape[0]
+
+    for index ,p in enumerate(proyeccion):
+        if p != 0:
+            index_floor = index
+            break
+
+    for index ,p in enumerate(proyeccion[::-1]):
+        if p != 0:
+            index_top = imagen.shape[0] - index
+            break
+
+    return index_top -index_floor
+
+def ancho_numero(imagen):
+    return alto_numero(imagen.T)
+
+
+
+
+
+
+
+
 
 
 
